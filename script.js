beff3de39d5828960c1d7da43fcade7c6c3f21d1
everaldooliveira2701 <everaldo2701@gmail.com>
2022-08-25 19:02:36 -0300

    inserção de js e reparo na função do botão

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+// const Modal = {            
+//     open: () => {  
+//         document.querySelector('.modal-overlay')
+//         .classList.add('active')
+//     },
+//     close: () => {
+//         document.querySelector('.modal-overlay')
+//         .classList.remove('active')
+//     }
+// }
+
+class ModalClass {
+    constructor(){}
+    open() {
+        document.querySelector('.modal-overlay')
+        .classList.add('active')
+    }
+}
 
 
+const Modal = new ModalClass();
+class Modal2Class {
+    constructor(){}
+    close() {
+        document.querySelector('.modal-overlay')
+        .classList.remove('active')
+    }
+}
 
+
+const Modal2 = new Modal2Class();
 
